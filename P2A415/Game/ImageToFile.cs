57f052f559c354dfbc1afdb1ece6b316b2e28565
HideFades <hiddenfades@gmail.com>
2016-03-31 10:52:43 +0200

    Comment updates on ImageToFile

--- P2A415/Game/ImageToFile.cs
+++ P2A415/Game/ImageToFile.cs
@@ -9,8 +9,8 @@ namespace WinFormsTest {
     class ImageToFile {
 
         public ImageToFile(string[,] arr) {
-            using (System.IO.StreamWriter MyFile = new System.IO.StreamWriter(@"")) {
-                for (int i = 0; i < arr.Length; i++) {
+            using (System.IO.StreamWriter MyFile = new System.IO.StreamWriter(@"")) {/*Input address for file*/
+                for (int i = 0; i < arr.Length; i++) { /*How do we determind size of each dimensional?*/
                     for (int j = 0; j < arr.Length; j++) {
                         MyFile.WriteLine(arr[i, j].ToString());
                     }
