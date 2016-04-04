a46a629c5f812a9d67e0bca45cef55d872ebd7f7
joakim <joakim347@gmail.com>
2016-04-04 13:59:55 +0200

    vs2015 er lort v2

---
+++ P2A415/Game/Dungeon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace WinFormsTest {
+    class Dungeon {
+        public TileType[,] DungoenTiles;
+        long x, y;
+        public int[] EntreCordinates = new int[2];
+
+        public Dungeon(long x, long y, int ex, int ey) {
+            this.x = x;
+            this.y = y;
+            CreateDungeon();
+
+            EntreCordinates[1] = ex; EntreCordinates[2] = ey;
+        }
+
+        public void CreateDungeon() {
+            DungoenTiles = new TileType[x, y];
+        }
+
+        public void DrawDungoen() {
+            
+        }
+
+
+    }
+}
