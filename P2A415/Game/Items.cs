0a3a77bb895e7168d310dcef8404dc8be944944f
HideFades <hiddenfades@gmail.com>
2016-04-25 14:24:30 +0200

    Itemlist

---
+++ P2A415/Game/Items.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTest {
+    public class Items {
+
+        public static Dictionary<int, Items> itemList = new Dictionary<int, Items>();
+
+        private static int itemID;
+        private string itemName;
+        private double itemHP;
+        private double itemLVL;
+        private double itemDMG;
+        private double itemDEF;
+
+        private struct itemType {
+            public int Hands;
+            public int Gloves;
+            public int Helmet;
+            public int Chest;
+            public int Belt;
+            public int Pants;
+            public int Boots;
+        }
+        private itemType equipSlot;
+
+        static Items() {
+            itemList.Add(itemID++, new Items() {
+                itemName = "Sword of Slays",
+                itemHP = 1,
+                itemLVL = 1,
+                itemDMG = 1,
+                itemDEF = 0,
+                equipSlot = new itemType() {
+                    Hands = 1
+                }
+            });
+            itemList.Add(itemID++, new Items() {
+                itemName = "The God's Gloves",
+                itemHP = 1,
+                itemLVL = 2,
+                itemDMG = 1,
+                itemDEF = 10,
+                equipSlot = new itemType() {
+                    Gloves = 1,
+                    Hands = -1
+                }
+            });
+            itemList.Add(itemID++, new Items() {
+                itemName = "Chest of big boobies",
+                itemHP = 1,
+                itemLVL = 3,
+                itemDMG = 3,
+                itemDEF = 2,
+                equipSlot = new itemType() {
+                    Chest = 1
+                }
+            });
+            itemList.Add(itemID++, new Items() {
+                itemName = "Tracer's leggings",
+                itemHP = 1,
+                itemLVL = 4,
+                itemDMG = 10,
+                itemDEF = 0,
+                equipSlot = new itemType() {
+                    Pants = 1
+                }
+            });
+        }
+
+    }
+}
