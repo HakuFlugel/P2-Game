6165a09553d38d2aba7e9f51ceeb0b4c10fc4dcd
Lighdek <matiasholst@hotmail.com>
2016-04-25 13:42:21 +0200

    Start of branch

---
+++ P2A415/Game/Road.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTest
+{
+    class Road
+    {
+
+        public Road(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; private set; }
+
+        public int Lenght => ParsePath().Length / 2;
+
+        public int[] End { get; private set; }
+
+        private int[] ParsePath()
+
+        {
+            string[] pathSection = Path.Split(';');
+
+            int[] parse = new int[pathSection.Length * 2]; //pathsection is a pair of coordinates
+
+            int jndex = 0;
+
+            foreach (string path in pathSection)
+            {
+                string[] temppath = path.Split(',');
+
+                parse[jndex] = int.Parse(temppath[0]);
+                parse[jndex + 1] = int.Parse(temppath[1]);
+
+                jndex += 2;
+            }
+
+            return parse;
+        }
+
+        public void AddPath(int[] coordinates, World region)
+        {
+            if (Path.Length == 0)
+                Path = string.Join(",", coordinates);
+            else
+                Path = Path + ";" + string.Join(",", coordinates);
+
+            End = coordinates;
+
+            region.guidgrid[coordinates[0], coordinates[1]] = 9999; //todo: change weight
+
+        }
+
+        public void AddRoadToGrid(World region)
+        {
+            int[] path = ParsePath();
+
+            for (int i = 0; i < path.Length; i += 2)
+            {
+                region[path[i], path[i + 1]] = "road"; //todo: fix plox
+            }
+        }
+
+        public int CheckCardinals(World region, out int[] newPath)
+        {
+            int direction = -1; // error number
+            int[] cardinals = new int[4];
+            newPath = new int[2];
+            //0 north, 1 east, 2 south, 3 west
+            try
+            {
+                cardinals[0] = region.guidgrid[End[0], End[1] + 1]; //todo: fetch weight
+            }
+            catch (IndexOutOfRangeException)
+            {
+                cardinals[0] = 10000;
+            }
+            try
+            {
+                cardinals[1] = region.guidgrid[End[0] + 1, End[1]]; //todo: fetch weight
+            }
+            catch (IndexOutOfRangeException)
+            {
+                cardinals[1] = 10000;
+            }
+            try
+            {
+                cardinals[2] = region.guidgrid[End[0], End[1] - 1]; //todo: fetch weight
+            }
+            catch (IndexOutOfRangeException)
+            {
+                cardinals[2] = 10000;
+            }
+            try
+            {
+                cardinals[3] = region.guidgrid[End[0] - 1, End[1]]; //todo: fetch weight
+            }
+            catch (IndexOutOfRangeException)
+            {
+                cardinals[3] = 10000;
+            }
+
+            int smallest = 9999; //just a high number
+            for (int index = 0; index < 4; index++)
+            {
+                if (cardinals[index] < smallest)
+                {
+                    smallest = cardinals[index];
+                    direction = index;
+                }
+            }
+            newPath = ChooseNewPath(direction);
+
+            return smallest;
+        }
+
+        private int[] ChooseNewPath(int direction)
+        {
+            int[] newpath = new int[2];
+            switch (direction)
+            {
+                case 0:
+                    newpath[0] = End[0];
+                    newpath[1] = End[1] + 1;
+                    break;
+                case 1:
+                    newpath[0] = End[0] + 1;
+                    newpath[1] = End[1];
+                    break;
+                case 2:
+                    newpath[0] = End[0];
+                    newpath[1] = End[1] - 1;
+                    break;
+                case 3:
+                    newpath[0] = End[0] - 1;
+                    newpath[1] = End[1];
+                    break;
+            }
+            return newpath;
+        }
+    }
+}
---
+++ P2A415/Game/RoadMaker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace WinFormsTest
+{
+    class RoadMaker
+    {
+        public static Dictionary<int[], Road> PathDictionary = new Dictionary<int[], Road>();
+
+        public RoadMaker(World region)
+        {
+            EndRegion = region;
+            TempRegion = new World();
+            TempRegion = EndRegion; //todo: ændre til value copy og ikke reference copy
+        }
+
+        public World EndRegion { get; set; }
+        public World TempRegion { get; set; }
+
+        public void MakeRoad(int[] start, int[] end)
+        {
+            Start = start;
+            End = end;
+            string path = "";
+            int[] gridSize = new int[2];
+            gridSize[0] = TempRegion.regions.GetLength(0);
+            gridSize[1] = TempRegion.regions.GetLength(1);
+            path = ProcessTiles(path);
+
+            if (path != "")
+            {
+                Road road = new Road(path);
+                road.AddRoadToGrid(EndRegion); //todo: somehow add road to region
+            }
+            else
+                Console.WriteLine("could not create road");
+        }
+
+        public int[] End { get; set; }
+
+        public int[] Start { get; set; }
+
+        private decimal Distance(int[] itemStart, int[] itemEnd)
+        {
+            int[] xy = new int[2];
+
+            xy[0] = itemStart[0] - itemEnd[0]; // x diff
+            xy[1] = itemStart[1] - itemEnd[1]; // y diff
+            decimal distance = (decimal)Math.Sqrt(xy[0] * xy[0] + xy[1] * xy[1]);
+
+            return distance;
+        }
+
+        private string ProcessTiles(string path)
+        {
+            Road startroad = new Road(path);
+            startroad.AddPath(Start, TempRegion);
+            PathDictionary.Add(startroad.End, startroad);
+            int[] newEnd = new int[2];
+
+            while (End[0] != newEnd[0] || End[1] != newEnd[1])
+            {
+                newEnd = ChooseNewRoad(out path);
+                Console.WriteLine(path);
+            }
+            return path;
+        }
+
+        private int[] ChooseNewRoad(out string path)
+        {
+            int best = 10000;
+            int[] key = new int[2];
+            int[] chosenRoad = new int[2];
+            foreach (KeyValuePair<int[], Road> item in PathDictionary)
+            {
+                int[] newRoad;
+                int newest = item.Value.CheckCardinals(TempRegion, out newRoad);
+                if (newest < best)
+                {
+                    best = newest;
+                    key = item.Key;
+                    chosenRoad = newRoad;
+                }
+            }
+
+            string tempPath = PathDictionary[key].Path;
+            PathDictionary.Add(chosenRoad, new Road(tempPath));
+            PathDictionary[chosenRoad].AddPath(chosenRoad, TempRegion);
+            path = PathDictionary[chosenRoad].Path;
+            return chosenRoad;
+        }
+    }
+}
