e512838f40b667015d9d757fd9e4c1e0920a30d1
joakim <joakim347@gmail.com>
2016-04-28 13:42:08 +0200

    division scale

--- P2A415/Game/Division.cs
+++ P2A415/Game/Division.cs
@@ -10,11 +10,11 @@ namespace WinFormsTest {
             this.operands = new int[rand.Next(2, 2 + (int)Math.Log10(level + 1))];
             int length = operands.Length;
 
-            correctAnswer = rand.Next(2, 2 + (int)Math.Log10(level + 1));
+            correctAnswer = rand.Next(2, 2 + (int)Math.Sqrt(level + 1));
 
             operands[0] = correctAnswer;
             for (int index = 1; index < length; index++) {
-                operands[index] = rand.Next(level, level*3);
+                operands[index] = rand.Next(2, 2 + (int)Math.Log(level + 1)*4);
                 operands[0] *= operands[index];
             }
 
