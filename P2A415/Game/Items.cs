3af62a22e8f1121e425280521ff94a9d81985ed1
GeneralDenmark <foo-fighters@live.dk>
2016-05-02 15:31:22 +0200

    Generating items!

--- P2A415/Game/Items.cs
+++ P2A415/Game/Items.cs
@@ -17,6 +17,8 @@ namespace RPGame {
         public double itemLVL;
         public double itemDMG;
         public double itemDEF;
+        public double itemSPEED;
+        public double itemPENE;
         public string flavortext;
 
         public struct itemType {
@@ -31,12 +33,28 @@ namespace RPGame {
         public itemType equipSlot;
 
         static Items() {
+
             itemList.Add(itemID++, new Items() {
                 itemImageFile = "",
-                itemName = "Sword of Slays",
-                itemHP = 1,
+                itemName = "Axe",
+                itemHP = 0,
                 itemLVL = 1,
-                itemDMG = 1,
+                itemDMG = 15,
+                itemPENE = 4,
+                itemDEF = 0,
+                itemSPEED = 0.25,
+                equipSlot = new itemType() {
+                    Hands = 2
+                }
+            });
+            itemList.Add(itemID++, new Items() {
+                itemImageFile = "",
+                itemName = "Claw",
+                itemHP = 0,
+                itemLVL = 1,
+                itemDMG = 12,
+                itemPENE = 4.55,
+                itemSPEED = 0.3,
                 itemDEF = 0,
                 equipSlot = new itemType() {
                     Hands = 1
@@ -44,42 +62,92 @@ namespace RPGame {
             });
             itemList.Add(itemID++, new Items() {
                 itemImageFile = "",
-                itemName = "God's Gloves",
-                itemHP = 1,
-                itemLVL = 2,
-                itemDMG = 1,
-                itemDEF = 10,
-                equipSlot = new itemType() {
-                    Gloves = 1,
-                    Hands = -1
-                }
-            });
-            itemList.Add(itemID++, new Items() {
-                itemImageFile = "",
-                itemName = "Chestplate of big boobies",
-                itemHP = 1,
-                itemLVL = 3,
-                itemDMG = 3,
-                itemDEF = 2,
-                equipSlot = new itemType() {
-                    Chest = 1
-                }
-            });
-            itemList.Add(itemID++, new Items() {
-                itemImageFile = "",
-                itemName = "Tracer's leggings",
-                itemHP = 1,
-                itemLVL = 4,
-                itemDMG = 10,
+                itemName = "Two Handed Sword",
+                itemHP = 0,
+                itemLVL = 1,
+                itemDMG = 13,
+                itemPENE = 1.4,
+                itemSPEED = 0.29,
                 itemDEF = 0,
                 equipSlot = new itemType() {
-                    Pants = 1
+                    Hands = 2
                 }
             });
+            itemList.Add(itemID++, new Items() {
+                itemImageFile = "",
+                itemName = "Club",
+                itemHP = 0,
+                itemLVL = 1,
+                itemDMG = 10,
+                itemPENE = 2,
+                itemSPEED = 0.375,
+                itemDEF = 0,
+                equipSlot = new itemType() {
+                    Hands = 2
+                }
+            });
+
+            itemList.Add(itemID++, new Items() {
+                itemImageFile = "",
+                itemName = "Sword",
+                itemHP = 0,
+                itemLVL = 1,
+                itemDMG = 11,
+                itemPENE = 1.75,
+                itemSPEED = 0.45,
+                itemDEF = 0,
+                equipSlot = new itemType() {
+                    Hands = 1
+                }
+            });
+            itemList.Add(itemID++, new Items() {
+                itemImageFile = "",
+                itemName = "Dagger",
+                itemHP = 0,
+                itemLVL = 1,
+                itemDMG = 8,
+                itemPENE = 1.5,
+                itemSPEED = 0.5,
+                itemDEF = 0,
+                equipSlot = new itemType() {
+                    Hands = 1
+                }
+            });
+
         }
-        public string nameToString(int index) {
-            return itemList[index].itemName;
+
+
+
+        private string[] titels = { "Wooden ", "Rusty ", "Blunt ", "Marked ", "Fugly ", "Ugly ", "Singing And Dancing ", "Longshot ", "Lickable ", "Beautifull ", "Outstanding ", "Understandable ", "Glory ", "Golden ", "Fine ", "Godly ", "Bloody ", "Sureal " };
+        //11
+
+        private string[] Endname = { " Of Destiny", " Made By God", " Of Slayer", " Slaien", ", Bob", ", Edgar", ", Hem", " Made Outta Gold", " Of What?", " Of Wood", " Of Leather", " Of Gold", ", Joakim" };
+        //13
+        private string[] flavorText = { "This ITEM is a long lost item. Now found, by you!", "Who is rdy for some seal clubbing?", "I like trains. And this ITEM. But I like trains more.", "ITEM is ready for your use, or missuse.", "This ITEM was once an adventure like you, but it took an arrow to the knee (Arrow, whats and arrow? -> they no excist in diz world)", "Oh My GOD! A dubble ITEM all the way, or something", "Flavor flavor flavor flavor", "I once had the chance, but moms spagettie got in the way", "Skov is Skov, but Skov is not skov" };
+
+        public Items MakeItem(Items item, int level) {
+            Random rand = new Random();
+            Items temp_item;
+            temp_item = item;
+            temp_item.itemName = (level < 20)? titels[rand.Next(0,titels.Count()/4)] :
+                                 (level < 50)? titels[rand.Next(0, titels.Count() / 2)] : titels[rand.Next(0, titels.Count())] +
+                                 item.itemName + Endname[rand.Next(0, Endname.Count())];
+
+            temp_item.itemDMG = temp_item.itemDMG * Math.Pow(1.07, level);
+
+            temp_item.itemHP = temp_item.itemHP * Math.Pow(1.07, level);
+
+            temp_item.itemHP = temp_item.itemDEF * Math.Pow(1.07, level);
+
+
+            temp_item.itemLVL = level;
+
+
+
+
+            return temp_item;
         }
+        
         public string hpToString(int index) {
             return "Health: " + itemList[index].itemHP.ToString();
         }
---
+++ P2A415/Game/Reward.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RPGame {
+    class Reward {
+        int dmg = 1;
+        int def = 1;
+        int hp = 1;
+
+
+
+        Reward(int monsterLevel) {
+            1.01
+
+
+        }
+
+
+        public void draw(Graphics gfx) {
+
+
+        }
+
+        public Items generating() {
+
+
+        }
+        
+    }
+}
